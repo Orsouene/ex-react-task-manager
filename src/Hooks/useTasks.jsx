1198df282627100cb17cbc4acc9ae77077812526
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-10 17:14:49 +0200

    Milestone 4 1° punto

---
+++ src/Hooks/useTasks.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+
+function useTasks() {
+      const [task, setTask] = useState([])
+        async function getTasks() {
+            try {
+                const fetchTasks = await fetch(url)
+                const data = await fetchTasks.json()
+                setTask(data)
+                console.log("recupero dei dati dal global context", data)
+            }
+            catch (error) {
+                console.log("errore nel recupero dei dati ",error)
+            }
+        }
+        useEffect(() => { getTasks() }, [])
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default useTasks
