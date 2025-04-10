4ece0ccc831096e660634e37cc4da8c39e092cc4
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-10 15:47:12 +0200

    Milestone 3 4° punto

--- src/Components/TaskRow.jsx
+++ src/Components/TaskRow.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function TaskRow({task}) {
   return (
-      <tr className='border ' >
-          <td className='w-62 pl-9 bg-amber-50'>{task.title}</td>
-          <td className='w-62 pl-25 bg-amber-100'>{task.status}</td>
-          <td className='w-62 pl-9 bg-amber-50'> {task.createdAt}</td>
+      <tr className='border text-center h-12 ' >
+          <td className='w-62  bg-amber-50 '>{task.title}</td>
+          <td className='w-62  bg-amber-100' style={{ color: task.status === "To do" ? "red" : task.status === "Doing" ?  "yellow" : "green" }}> <span className='bg-stone-800 text-sm p-2 border-2  border-white rounded-2xl'>{task.status}</span> </td>
+          <td className='w-62  bg-amber-50'> {task.createdAt}</td>
       </tr>
   )
 }
