78128bc3f45556603bf247483c3be27ec504e31b
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-10 12:50:37 +0200

    Milestone 3 3° punto

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useContext } from 'react'
+import { GlobalContext } from '../Context/GlobalContext'
 function TaskList() {
+  const tasks = useContext(GlobalContext)
   return (
     <div>
-      Welcome to taskList
+      
     </div>
   )
 }
