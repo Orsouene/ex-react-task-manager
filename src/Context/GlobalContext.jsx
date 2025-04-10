ed6903f96a5becc04079af42f1a3637eb0c849b4
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-10 12:23:27 +0200

    Milestone 2 4° punto

--- src/Context/GlobalContext.jsx
+++ src/Context/GlobalContext.jsx
@@ -1,7 +1,19 @@
 import React, { createContext, useState } from 'react'
 const GlobalContext=createContext()
 function GlobalProvider() {
-    const [tasks,setTasks]=useState([])
+    const [task,setTask]=useState([])
+    async function getTasks() {
+        try {
+            const fetchTasks = await fetch(url)
+            const data = await fetchTasks.json()
+            setTask(data)
+           
+        }
+        catch (error) {
+            console.log("errore nel recupero dei dati ")
+        }
+    }
+    useEffect(() => { getTasks() }, [])
   return (
     <div>
       
