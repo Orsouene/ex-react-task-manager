0d52cea421ff6cf216e147de5051a396bb679054
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-10 12:19:26 +0200

    Milestone 3° punto

--- src/Context/GlobalContext.jsx
+++ src/Context/GlobalContext.jsx
@@ -1,6 +1,7 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 const GlobalContext=createContext()
 function GlobalProvider() {
+    const [tasks,setTasks]=useState([])
   return (
     <div>
       
