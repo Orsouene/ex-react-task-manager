b135304da48706f09f933059e2cfe69f1b0cddac
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-10 12:16:04 +0200

    Milestone 2 2°  punto

---
+++ src/Context/GlobalContext.jsx
@@ -0,0 +1,11 @@
+import React, { createContext } from 'react'
+const GlobalContext=createContext()
+function GlobalProvider() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export  {GlobalProvider,GlobalContext}
