c5e64a66c8aa7663e8aa9e667c1cbfd8ec9930dd
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-11 16:10:57 +0200

    Milestone 5 1° punto

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState,useRef, useEffect  } from 'react'
 
 function AddTask() {
+
+  const refDescrizione=useRef("")
+  const refStato =useRef("")
+
+  //* il stato del form inizialmente
+  const [formTitle, setFormTitle] = useState("")
+  //* funzione per gestire il form
+  const handleForm = (e) => {
+    e.preventDefault()
+  
+  }
+
   return (
-    <div>
-      Welcome to addtask
+    <div className='border-2  border-amber-800 mt-10  gap-1 max-w-96 m-auto border-b-8 rounded-r-2xl h-96'>
+      <form className='flex flex-col gap-3 p-2' onSubmit={handleForm} >
+        {/* Title */}
+
+        <label htmlFor="Status">title</label>
+        <input type="text" name='title' value={formTitle} onChange={e => setFormTitle(e.target.value)} className='border rounded-2xl p-1 text-sm'/>
+
+        {/* Description */}
+        <label htmlFor="Description">Description</label>
+        <textarea type="text" name='description'ref={refStato} className='border rounded-2xl p-1 text-sm' />
+        {/* Status */}
+        <label htmlFor="Description">Status</label>
+        <select type="text" name='status' ref={refDescrizione} className='border rounded-2xl p-1 text-sm' defaultValue={"To do"} >
+          <option value="To do">To do</option>
+          <option value="Doing">Doing</option>
+          <option value="Done"> Done</option>
+
+        </select>
+
+        {/* Button */}
+        <button type='submit' className=' w-fit m-auto  border  border-amber-800  p-2 rounded-2xl border-b-4 border-l-2 cursor-pointer hover:bg-orange-400 hover:text-white active:border-b-1 active:border-l-1'>Submit</button>
+      </form>
+
     </div>
   )
 }
