419216e2c47d1c49082f5e1292516cde119bc8ad
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-11 17:41:39 +0200

    Fine milestone 5

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,15 +1,22 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react'
+import React, { useState, useRef,useMemo } from 'react'
 
 function AddTask() {
-
-  const refDescrizione = useRef("")
-  const refStato = useRef("")
   //* il stato del form inizialmente
   const [formTitle, setFormTitle] = useState("")
+  const refDescrizione = useRef("")
+  const refStato = useRef("")
+const myForm= {
+ title : formTitle,
+  descrizione :  refDescrizione.current.value, 
+  stato :  refStato.current.value }
+
+  
+
+
   //* funzione per gestire il form
   const handleForm = (e) => {
            e.preventDefault()
-         
+         console.log(myForm)
   }
   const controllCamp = useMemo( () => {
         const symbols = "'!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~"
@@ -32,19 +39,18 @@ function AddTask() {
 
   return (
     <div className='border-2  border-amber-800 mt-10  gap-1 max-w-96 m-auto border-b-8 rounded-r-2xl h-96'>
-          <form className='flex flex-col gap-3 p-2' onSubmit={handleForm} >
+          <form className='flex flex-col gap-3 p-2 ' onSubmit={handleForm}  >
             {/* Title */}
-
-                      <label htmlFor="Status">title</label>
+                
+                    <label htmlFor="Status">title</label>
                       <input type="text" name='title' value={formTitle} onChange={e => setFormTitle(e.target.value)} className='border rounded-2xl p-1 text-sm' />
                       <span className='text-red-600 text-xs'>{controllCamp}</span>
-
                       {/* Description */}
                       <label htmlFor="Description">Description</label>
-                      <textarea type="text" name='description' ref={refStato} className='border rounded-2xl p-1 text-sm' />
+                      <textarea type="text" name='description' ref={refDescrizione} className='border rounded-2xl p-1 text-sm' />
                       {/* Status */}
-                      <label htmlFor="Description">Status</label>
-                      <select type="text" name='status' ref={refDescrizione} className='border rounded-2xl p-1 text-sm' defaultValue={"To do"} >
+                     <label htmlFor="status">Status</label>
+                      <select type="text" name='status' ref={refStato} className='border rounded-2xl p-1 text-sm' defaultValue={"To do"} >
                         <option value="To do">To do</option>
                         <option value="Doing">Doing</option>
                         <option value="Done"> Done</option>
@@ -52,7 +58,7 @@ function AddTask() {
                       </select>
 
                       {/* Button */}
-                      <button type='submit' className=' w-fit m-auto  border  border-amber-800  p-2 rounded-2xl border-b-4 border-l-2 cursor-pointer hover:bg-orange-400 hover:text-white active:border-b-1 active:border-l-1'>Submit</button>
+        <button type='submit' className=' w-fit m-auto  border  border-amber-800  p-2 rounded-2xl border-b-4 border-l-2 cursor-pointer hover:bg-orange-400 hover:text-white active:border-b-1 active:border-l-1 disabled:opacity-50 disabled:border-b-1 disabled:border-l-1 disabled:not-hover:bg-orange-400 disabled:not-hover:text-white disabled:cursor-not-allowed' disabled={controllCamp}>Submit</button>
           </form>
 
     </div>
