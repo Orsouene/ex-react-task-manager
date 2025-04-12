0756bced0f7130d78bc7eda2f8b3457456d5294f
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-12 11:41:55 +0200

    Milestone 7 1° punto

--- src/Components/TaskRow.jsx
+++ src/Components/TaskRow.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 const TaskRow =React.memo(({task})=> {
     console.log("Rendering TaskRow", task.id); 
   return (
       <tr className='border text-center h-12 font-bold' >
         
-          <td className='w-62  bg-amber-50 '>{task.title}</td>
+      <td className='w-62  bg-amber-50 '> <NavLink to={`/${task.id}`}> {task.title}</NavLink> </td>
       <td className='w-62  bg-amber-100' style={{ backgroundColor: task.status === "To do" ? "#dc2626" : task.status === "Doing" ? "#fde047" : "#16a34a" }}> <span className='text-sm p-2 border-2  border-white rounded-2xl bg-amber-50'>{task.status}</span> </td>
           <td className='w-62  bg-amber-50 '> 
             { new Date(task.createdAt).toLocaleDateString()}</td>
