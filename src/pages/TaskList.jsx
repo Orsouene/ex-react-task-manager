d7faf04745e44de469e12ab9235449657800dda3
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-15 20:06:44 +0200

    Milestone 11 2° punto

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -6,17 +6,37 @@ function TaskList() {
   const {tasks} = useContext(GlobalContext)
   const [sortBy, setSortBy] = useState("createdAt")
   const [sortOrder, setSortOrder] = useState(1)
-  
+  const icon = sortOrder=== 1? " ⬆" : "⬇"
+
+  const handleSort = (campo)=>{
+    // console.log(campo)
+         if(sortBy===campo)
+          setSortOrder(prev=>prev*-1)
+  else {
+      setSortBy(campo) 
+      setSortOrder(1)
+  }
+  }
   return (
     <div className='w-fit m-auto mt-20  p-20 rounded-2xl border-stone-800 border border-b-8 border-r-5 '>
     
             <table >
-                        <thead className='border w-fit m-auto '>
-                              <tr className='bg-amber-100'>
-                                    <th>Nome</th>
-                                    <th>Stato</th>
-                                    <th>Data di Creazione</th>
-                              </tr>
+                        <thead className='border w-fit m-auto cursor-pointer '>
+                                  <tr className='bg-amber-100 '>
+                                          <th onClick={() => handleSort("title")}>
+                                              Nome {sortBy ==="title" ? icon :""}
+                                        </th>
+                                          <th onClick={() => handleSort("status")}>
+                                              Stato {sortBy === "status" ? icon : ""}
+                                          </th>
+                                        <div className='flex justify-center gap-1'>
+                                            <th onClick={() =>  handleSort("createdAt")} > 
+                                              Data di Creazione 
+                                           </th>
+                                            {sortBy === "createdAt" ? icon : ""}
+                                        </div>
+                                        
+                                  </tr>
                         </thead>
                 
                         <tbody className='border ' >
