acf0ba42083b321e261d115c8651a6d5dd62e594
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-16 18:50:08 +0200

    milestone 12 2° punto

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -23,11 +23,9 @@ function TaskList() {
 
 //* ordine del array
    const arrayOrdinato =useMemo(()=>{
-    let orderStatus = {
-      "To do" : 0,
-      "Doing":1,
-      "Done":2
-    }
+    let orderStatus = [ "To do" ,
+      "Doing",
+      "Done"]
 
  //!by-title
     if (sortBy==="title"){
@@ -46,10 +44,10 @@ function TaskList() {
  //!by-Status
      if (sortBy === "status") { 
         if (sortOrder === 1) {
-        tasks.sort((a, b) =>  { return  orderStatus[a.status] - orderStatus[b.status]})
+        tasks.sort((a, b) =>  { return  orderStatus.indexOf(a.status) - orderStatus.indexOf(b.status)})
      }
      else {
-          return tasks.sort((a, b) => orderStatus[b.status] - orderStatus[a.status])
+          tasks.sort((a, b) => { return orderStatus.indexOf(b.status) - orderStatus.indexOf(a.status) })
      }}
 
 //!by-createdAt
@@ -61,27 +59,37 @@ function TaskList() {
       }
        else {
           tasks.sort((a, b) => {
-          return     new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          return  new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
          })
        }     
     }
-     return tasks
-   }, [tasks,sortBy,sortOrder])
+     
+//!  Filtrare il resultato  per la ricerca 
+     const filtredArray = tasks.filter(
+       el => 
+         el.title.toLowerCase().includes(searchQuery.toLowerCase()) || el.status.toLowerCase().includes(searchQuery.toLowerCase()) || (new Date(el.createdAt).toLocaleDateString()).includes(searchQuery)
+     
   
-  
+                                   )
+
+ return filtredArray
+   }, [tasks, sortBy, sortOrder, searchQuery])
+
+
    return (
     <div className='w-fit m-auto mt-20  p-20 rounded-2xl border-stone-800 border border-b-8 border-r-5 '>
-       <label htmlFor="" className='flex gap-1 mb-2 justify-center border-2 rounded-2xl  w-fit border-amber-200 m-auto p-2 border-b-4 border-r-4'>
-         <span className='border p-1'>Search</span>
-         <input className='border ' />
+       <label htmlFor="" className='flex gap-1 mb-2 justify-center border-2 rounded-2xl  w-fit border-stone-800 m-auto p-2 border-b-4 border-r-4'>
+         <span className='border p-1' onClick={()=>search()}>Search</span>
+         <input className='border ' value={searchQuery} onChange={e=>setSearchQuery(e.target.value)} />
+         <button className='cursor-pointer border p-1' onClick={()=>setSearchQuery("")}>Reset</button>
       </label>
     
-            <table >
-        <thead className='border w-fit m-auto cursor-pointer '>
+    <table >
+                       <thead className='border w-fit m-auto cursor-pointer '>
                                   <tr className='bg-amber-100 '>
-                         <th onClick={() => handleSort("title")}>
-                                              Nome {sortBy ==="title" ? icon :""}
-                                        </th>
+                                          <th onClick={() => handleSort("title")}>
+                                                              Nome {sortBy ==="title" ? icon :""}
+                                          </th>
                                           <th onClick={() => handleSort("status")}>
                                               Stato {sortBy === "status" ? icon : ""}
                                           </th>
@@ -96,10 +104,20 @@ function TaskList() {
                                   </tr>
                         </thead>
                 
-                        <tbody className='border ' >
-          {arrayOrdinato.map((task) => (  <TaskRow key={task.id} task={task}/> ))}  
-                        </tbody> 
-            </table> 
+                          <tbody className='border ' >
+                                { arrayOrdinato.length ? arrayOrdinato.map((task) => (  <TaskRow key={task.id} task={task}/> ))
+                              : 
+                                  <tr>
+                                      <td colSpan="100%" className="text-red-800 font-bold text-center">
+                                        Nessun Task trovato..
+                                      </td>
+                                    </tr>
+                                          
+                                      }
+                              
+                          </tbody> 
+                                            
+      </table> 
     
     </div>
   )
