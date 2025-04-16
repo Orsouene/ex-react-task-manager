e0e56d004a88441f329fc1565ee85ca60456721a
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-16 11:45:27 +0200

    Fine milestone-11

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useMemo, useState } from 'react'
 import { GlobalContext } from '../Context/GlobalContext'
 import TaskRow from '../Components/TaskRow'
 function TaskList() {
 
   const {tasks} = useContext(GlobalContext)
+
   const [sortBy, setSortBy] = useState("createdAt")
   const [sortOrder, setSortOrder] = useState(1)
   const icon = sortOrder=== 1? " ⬆" : "⬇"
@@ -17,30 +18,76 @@ function TaskList() {
       setSortOrder(1)
   }
   }
+   const ordinamento =useMemo(()=>{
+    let orderStatus = {
+      "To do" : 0,
+      "Doing":1,
+      "Done":2
+    }
+
+ //!by-title
+    if (sortBy==="title"){
+             {
+              if (sortOrder===1 )
+             tasks.sort((a, b) => { 
+             return      a.title.localeCompare(b.title) 
+              })
+              else {
+                 tasks.sort((a, b) => {
+               return    b.title.localeCompare(a.title)
+                })
+              }
+            }
+    }
+ //!by-Status
+     if (sortBy === "status") { 
+        if (sortOrder === 1) {
+        tasks.sort((a, b) =>  { return  orderStatus[a.status] - orderStatus[b.status]})
+     }
+     else {
+          return tasks.sort((a, b) => orderStatus[b.status] - orderStatus[a.status])
+     }}
+
+//!by-createdAt
+     if (sortBy === "createdAt") {
+        if (sortOrder === 1) {
+        tasks.sort((a, b) => {
+           return    new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+         })
+      }
+       else {
+          tasks.sort((a, b) => {
+          return     new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+         })
+       }     
+    }
+     return tasks
+   }, [tasks,sortBy,sortOrder])
   return (
     <div className='w-fit m-auto mt-20  p-20 rounded-2xl border-stone-800 border border-b-8 border-r-5 '>
     
             <table >
-                        <thead className='border w-fit m-auto cursor-pointer '>
+        <thead className='border w-fit m-auto cursor-pointer '>
                                   <tr className='bg-amber-100 '>
-                                          <th onClick={() => handleSort("title")}>
+                         <th onClick={() => handleSort("title")}>
                                               Nome {sortBy ==="title" ? icon :""}
                                         </th>
                                           <th onClick={() => handleSort("status")}>
                                               Stato {sortBy === "status" ? icon : ""}
                                           </th>
-                                        <div className='flex justify-center gap-1'>
+                                     
                                             <th onClick={() =>  handleSort("createdAt")} > 
                                               Data di Creazione 
+                                           {sortBy === "createdAt" ? icon : ""} 
                                            </th>
-                                            {sortBy === "createdAt" ? icon : ""}
-                                        </div>
+                                           
+                                      
                                         
                                   </tr>
                         </thead>
                 
                         <tbody className='border ' >
-                            {tasks.map((task) => (  <TaskRow key={task.id} task={task}/> ))}  
+          {ordinamento.map((task) => (  <TaskRow key={task.id} task={task}/> ))}  
                         </tbody> 
             </table> 
     
